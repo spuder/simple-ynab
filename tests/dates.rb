c957bd211cb126716ee799073be352b3c1e2bb3f
Spencer Owen <owenspencer@gmail.com>
2014-11-14 12:34:18 -0700

    Adds support for converting dates Improves log messages and adds more colors to -v output

---
+++ tests/dates.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+
+require 'Date'
+# Test for converting the date fromat from YYYY-MM-DD to DD-MM-YYYY for ynab simplicity
+
+foo = ["2014/11/12", "2014-11-12 03:06:38 PM -0700", nil, "-6.44", "Signature purchase", "true", "WENDYS #4850", "Wendys", "Food & Drink", "Fast Food", nil, nil, nil, nil, nil, nil, nil]
+
+puts ""
+puts "foo[0] is #{foo[0]}"
+
+foodate = Date.parse(foo[0])
+# puts "#{Date.parse foo[0]}"
+
+puts foodate.strftime(format='%d''/''%m''/''%Y')
+
+puts "changing the format"
+
+puts "#{foo[0] = Date.parse(foo[0]).strftime(format='%D').to_s}"
+puts "foo[0] is now #{foo[0]}"
+
+p foo
